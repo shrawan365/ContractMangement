d7f4b7cb78efc7be5ff6be6659ebc8474dc00fbf
Komal Parik <kp8263262@gmail.com>
2022-11-19 17:36:01 +0530

    Account Login Completed

--- ContractMangement/Controllers/AccountController.cs
+++ ContractMangement/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContractMangement.Models.CustomModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,18 @@ namespace ContractMangement.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel login)
+        {
+            if(login.Password =="admin" && login.Username=="admin" ) {
+                return RedirectToAction("Index","Home");
+            }
+            else {
+                login.Username = "";
+                ViewBag.error = "Incorrect Username or Password";
+                return View(login);
+            }
         }
         public ActionResult Singup()
         {
---
+++ ContractMangement/Models/CustomModel/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContractMangement.Models.CustomModel
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string error { get; set; }
+    }
+}
